18b550fc50ff7bfbc6a4778854d20a5830b12a9c
marwal <mariusz.waldoch98@gmail.com>
2023-04-18 21:48:00 +0200

    Add some styles to MoviesList and create dummy data for movies list

--- components/MoviesList/MoviesList.tsx
+++ components/MoviesList/MoviesList.tsx
@@ -1,5 +1,14 @@
+import { Box, List, Typography } from "@mui/material";
+
+import {StyledListWrapper} from './MoviesList.styled';
+
 const MoviesList: React.FC = () => {
-    return <></>
-}
+  return (
+    <StyledListWrapper>
+      <Typography variant="h6" component="div">Popular movies</Typography>
+      <List></List>
+    </StyledListWrapper>
+  );
+};
 
 export default MoviesList;
