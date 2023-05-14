5564db8a405ce70f4368c9b26bd178a62a7557a4
marwal <mariusz.waldoch98@gmail.com>
2023-05-14 20:59:57 +0200

    Make user details page responsive

--- components/UserDetails/UserDetails.styled.ts
+++ components/UserDetails/UserDetails.styled.ts
@@ -3,4 +3,9 @@ import { styled, Box } from "@mui/material";
 export const StyledUserDetailsWrapper = styled(Box)(({ theme }) => ({
   display: "flex",
   justifyContent: "space-evenly",
+
+  [theme.breakpoints.down("md")]: {
+    display: "flex",
+    flexDirection: "column",
+  },
 }));
