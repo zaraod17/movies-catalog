1ac0bab6789d18cf56b00a4e5f62901f6d252ce0
marwal <mariusz.waldoch98@gmail.com>
2023-05-28 18:41:55 +0200

    Destructure api code

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,18 +1,10 @@
-import { gql, ApolloServer } from "apollo-server-micro";
+import { ApolloServer } from "apollo-server-micro";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+import { resolvers } from "@/utils/ApiResolvers";
+import { typeDefs } from "@/utils/ApiSchema";
 
-const resolvers = {
-  Query: {
-    hello: () => "Hello, GraphQL!",
-  },
-};
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
@@ -33,6 +25,6 @@ export default async function handler(
 
 export const config = {
   api: {
-    bodyParser: false
-  }
+    bodyParser: false,
+  },
 };
