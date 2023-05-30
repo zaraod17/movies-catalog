772f0c8ab10891b8f30a14b4a07c2e0977fdbf81
marwal <mariusz.waldoch98@gmail.com>
2023-05-30 17:39:01 +0200

    Add apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import MainLayout from "@/layouts/MainLayout";
 
+const client = new ApolloClient({
+  uri: `${window.location.origin}/api/graphql`,
+  cache: new InMemoryCache(),
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <ApolloProvider client={client}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ApolloProvider>
   );
 }
