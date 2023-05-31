428d336df7be3240cc90b18e82eb8278ed3ad703
marwal <mariusz.waldoch98@gmail.com>
2023-05-31 19:57:43 +0200

    Implement apollo client fetching

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { useRouter } from "next/router";
 
 import MainLayout from "@/layouts/MainLayout";
 
-const client = new ApolloClient({
-  uri: `${window.location.origin}/api/graphql`,
-  cache: new InMemoryCache(),
-});
-
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const client = new ApolloClient({
+    uri: `${router.basePath}/api/graphql`,
+    cache: new InMemoryCache(),
+  });
+
   return (
     <ApolloProvider client={client}>
       <MainLayout>
