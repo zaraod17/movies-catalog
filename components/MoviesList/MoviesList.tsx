3bbc5b6cb5d2a2aeb82ff4702544bf2c5b5855ce
marwal <mariusz.waldoch98@gmail.com>
2023-06-01 17:36:24 +0200

    Add data fetching to MoviesList component

--- components/MoviesList/MoviesList.tsx
+++ components/MoviesList/MoviesList.tsx
@@ -1,13 +1,30 @@
-import { List, Typography } from "@mui/material";
+import { List, Typography, CircularProgress } from "@mui/material";
 import { useQuery } from "@apollo/client";
 
 import { StyledListWrapper, StyledLink } from "./MoviesList.styled";
 import MoviesListElement from "./MoviesListElement/MoviesListElement";
 
 import { DUMMY_MOVIES } from "@/data/dummy-data";
+import { ListTitleType } from "@/components/MainPageLists/MainPageList.types";
+import {
+  GET_POPURLAR_MOVIES,
+  GET_LATEST_RELEASES,
+  GET_UPCOMING_MOVIES,
+} from "@/utils/ApiClientQueries";
 
-const MoviesList: React.FC<{ listTitle: string }> = ({ listTitle }) => {
-  // const {loading, error, data} = useQuery();
+const MoviesList: React.FC<{ listTitle: ListTitleType }> = ({ listTitle }) => {
+  const query =
+    listTitle === "Popular Movies"
+      ? GET_POPURLAR_MOVIES
+      : listTitle === "Latest Releases"
+      ? GET_LATEST_RELEASES
+      : GET_UPCOMING_MOVIES;
+
+  const { loading, error, data } = useQuery(query);
+
+  if (loading) {
+    return <CircularProgress />;
+  }
 
   return (
     <StyledListWrapper>
--- utils/ApiClientQueries.ts
+++ utils/ApiClientQueries.ts
@@ -25,3 +25,39 @@ export const GET_POPURLAR_MOVIES = gql`
     }
   }
 `;
+
+export const GET_LATEST_RELEASES = gql`
+  query Query {
+    getLatestReleases {
+      id
+      title
+      imgUrl
+      productionYear
+      sumOfRatings
+      numberOfRatings
+      description
+      trailerUrl
+      actors
+      categories
+      views
+    }
+  }
+`;
+
+export const GET_UPCOMING_MOVIES = gql`
+  query Query {
+    getUpcomingMovies {
+      id
+      title
+      imgUrl
+      productionYear
+      sumOfRatings
+      numberOfRatings
+      description
+      trailerUrl
+      actors
+      categories
+      views
+    }
+  }
+`;
