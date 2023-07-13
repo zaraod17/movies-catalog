26c6622f27097eba853ca133f9c06800c9520560
marwal <mariusz.waldoch98@gmail.com>
2023-07-13 20:40:04 +0200

    Implement simple client route protection

--- pages/user-details/index.tsx
+++ pages/user-details/index.tsx
@@ -1,6 +1,20 @@
+import { useContext, useEffect } from "react";
+import {  useRouter } from "next/router";
+
 import UserDetails from "@/components/UserDetails/UserDetails";
 
+import { AuthContext } from "@/contexts/AuthContext";
+
 const UserDetailsPage: React.FC = () => {
+  const router = useRouter();
+  const { token, userInfo } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!(token && userInfo.email)) {
+      router.replace('/');
+    }
+  }, []);
+
   return (
     <>
       <UserDetails />
