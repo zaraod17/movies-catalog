7038feb81faf1edf62e8fd78188888157a9fc60c
marwal <mariusz.waldoch98@gmail.com>
2023-07-16 14:50:14 +0200

    Remove unnecessary auth checks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,6 @@
 import MainPageLists from "@/components/MainPageLists/MainPageLists";
-import { useContext, useEffect } from "react";
-import { useRouter } from "next/router";
-
-import { AuthContext } from "@/contexts/AuthContext";
 
 const Home: React.FC = () => {
-  const router = useRouter();
-  const { token, userInfo } = useContext(AuthContext);
-
-  useEffect(() => {
-    if (!(token && userInfo.email)) {
-      router.replace("/");
-    }
-  }, [router, token, userInfo.email]);
   return (
     <>
       <MainPageLists />
--- pages/movie-details/[movieId].tsx
+++ pages/movie-details/[movieId].tsx
@@ -2,20 +2,7 @@ import MovieDetails from "@/components/MovieDetails/MovieDetails";
 
 import { GetServerSideProps } from "next/types";
 
-import { useContext, useEffect } from "react";
-import { useRouter } from "next/router";
-
-import { AuthContext } from "@/contexts/AuthContext";
-
 const MovieDetailsPage: React.FC<{ id: number | string }> = ({ id }) => {
-  const router = useRouter();
-  const { token, userInfo } = useContext(AuthContext);
-
-  useEffect(() => {
-    if (!(token && userInfo.email)) {
-      router.replace("/");
-    }
-  }, [router, token, userInfo.email]);
   return (
     <>
       <MovieDetails id={id} />
