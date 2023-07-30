705da1d40dca758973b0def560f5242af01936d9
marwal <mariusz.waldoch98@gmail.com>
2023-07-30 16:20:00 +0200

    api route test 2

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,13 +5,13 @@ type Data = {
   name: string;
 };
 
-export const config = {
-  type: "experimental-background",
-};
-
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   res.status(200).json({ name: "John Doe" });
 }
+
+export const config = {
+  type: "experimental-background",
+};
